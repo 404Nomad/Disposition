5f5213eea0397ecdcfe238b66824898351bb91a9
404Nomad <richtofen60@gmail.com>
2025-02-28 16:07:34 +0100

    new package model et data, new dataclass DrawableStringPair dans model,

---
+++ app/src/main/java/com/cfa/disposition/model/DrawableStringPair.kt
@@ -0,0 +1,9 @@
+package com.cfa.disposition.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
