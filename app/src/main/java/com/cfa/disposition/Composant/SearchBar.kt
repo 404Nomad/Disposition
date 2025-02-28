947cc9ad3bbf13e48b65d0d45e71b93337b0f501
404Nomad <richtofen60@gmail.com>
2025-02-28 16:31:33 +0100

    ajout champ de recherche

--- app/src/main/java/com/cfa/disposition/Composant/SearchBar.kt
+++ app/src/main/java/com/cfa/disposition/Composant/SearchBar.kt
@@ -1,11 +1,19 @@
 package com.cfa.disposition.Composant
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.cfa.disposition.R
 
 @Composable
 fun SearchBar(
@@ -19,7 +27,16 @@ fun SearchBar(
                 imageVector = Icons.Default.Search,
                 contentDescription = null
             )
-        }
+
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        modifier = modifier.fillMaxWidth().heightIn(min = 56.dp)
 
     )
 }
