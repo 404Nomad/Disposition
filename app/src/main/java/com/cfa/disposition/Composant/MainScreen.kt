2e683e7817d7183830d417cf13d78d37d9066d2f
404Nomad <richtofen60@gmail.com>
2025-02-28 16:23:43 +0100

    ajout padding de la searchbar

--- app/src/main/java/com/cfa/disposition/Composant/MainScreen.kt
+++ app/src/main/java/com/cfa/disposition/Composant/MainScreen.kt
@@ -3,6 +3,7 @@ package com.cfa.disposition.Composant
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
@@ -19,7 +20,7 @@ fun MainScreen(
     ) {
         //forcer espace au dessus et en dessous
         Spacer(Modifier.height(16.dp))
-        SearchBar()
+        SearchBar(Modifier.padding(16.dp))
         Text("en attendant les composants")
         Spacer(Modifier.height(16.dp))
     }
